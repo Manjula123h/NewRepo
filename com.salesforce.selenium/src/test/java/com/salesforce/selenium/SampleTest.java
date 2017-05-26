9e6bcabaf7fae6d9c4ce161428bc9f4123803a7d
ab86549 <ab86549@L900253.ctl.intranet>
2017-05-26 11:54:55 +0530

    Commit by Manjula

---
+++ com.salesforce.selenium/src/test/java/com/salesforce/selenium/SampleTest.java
@@ -0,0 +1,24 @@
+package com.salesforce.selenium;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.testng.annotations.Test;
+
+
+public class SampleTest {
+
+@Test
+	public void test()
+		
+
+	{
+		System.out.println("Hello");
+		System.out.println("Welcome To Git World");
+		System.setProperty("webdriver.chrome.driver", "C:\\ChromeDriver\\chromedriver_win32\\chromedriver.exe");
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://www.google.co.in/");
+	}
+	}
+
