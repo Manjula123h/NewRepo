3ea08e7935d5f0f08e983ecb6780326c36794216
ab86549 <ab86549@L900253.ctl.intranet>
2017-05-26 12:13:23 +0530

    Commit by Manjula Updated GitClass

---
+++ com.salesforce.selenium/src/test/java/com/salesforce/selenium/GitClass.java
@@ -0,0 +1,11 @@
+package com.salesforce.selenium;
+
+public class GitClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("HIIIIIIIIIIIIIIII");
+
+	}
+
+}
